50d1b6f2dbea189d31d00a5a17d635637609443d
Nabil Fannane <nabil@goadroit.com>
2020-02-08 01:05:27 -0800

    last

--- MealFinderAPI/main.js
+++ MealFinderAPI/main.js
@@ -1,41 +1,51 @@
 const form = document.querySelector('#form'),
     search = document.querySelector('#search'),
     submit = document.querySelector('.search-btn'),
-    random = document.querySelector('.random-btn')
+    random = document.querySelector('#random')
 meals = document.querySelector('#meals'),
     result = document.querySelector('#result-heading')
 mealResult = document.querySelector('#meal'),
     error = document.querySelector('#error'),
 
+    random.addEventListener('click', e => {
+        meals.innerHTML = ''
+        // mealResult = innerHTML = ''
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+            .then(res => res.json())
+            .then(data => {
+                let meal = data.meals[0]
+                addMealToDOM(meal)
+            })
+    })
 
 
 
+// Event listeners
 
-    // Event listeners
-
-    form.addEventListener('submit', e => {
-        e.preventDefault()
-
-
-        meal.innerHTML = ''
+form.addEventListener('submit', e => {
+    e.preventDefault()
 
 
-        const term = search.value
+    meal.innerHTML = ''
 
-        if (term.trim()) {
 
-            const res = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    result.innerHTML = `<h2>Search result fro '${term}':</h2>`
-                    if (data.meals === null) {
-                        result.innerHTML = `<p class="null">There is no search result. Try again !!</p>`
-                        setTimeout(() => {
-                            result.innerHTML = ''
-                        }, 1000)
-                    } else {
-                        meals.innerHTML = data.meals.map(meal => `
+    const term = search.value
+
+    if (term.trim()) {
+
+        const res = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                result.innerHTML = `<h2>Search result fro '${term}':</h2>`
+                if (data.meals === null) {
+                    result.innerHTML = `<p class="null">There is no search result. Try again !!</p>`
+                    setTimeout(() => {
+                        result.innerHTML = ''
+                    }, 1000)
+                } else {
+                    meals.innerHTML = data.meals.map(meal => `
                          <div class="meal">
                          <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
                          <div class="meal-info" data-mealID="${meal.idMeal}">
@@ -44,17 +54,17 @@ mealResult = document.querySelector('#meal'),
                          </div>
                         `)
 
-                    }
-                })
-            search.value = ''
+                }
+            })
+        search.value = ''
 
-        } else {
-            error.style.opacity = 1
-            setTimeout(() => {
-                error.style.opacity = 0
-            }, 1500)
-        }
-    })
+    } else {
+        error.style.opacity = 1
+        setTimeout(() => {
+            error.style.opacity = 0
+        }, 1500)
+    }
+})
 const getMealbyID = id => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
         .then(res => res.json())
