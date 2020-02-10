67ccb7fccbae18ecc2088d75ed342d22880957a9
Nabil Fannane <nabil@goadroit.com>
2020-02-10 05:16:17 -0800

    last

---
+++ infiniteScrolling/main.js
@@ -0,0 +1,49 @@
+const postContainer = document.querySelector(".post-container");
+const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
+
+let limit = 3;
+let page = 1;
+
+async function fetchPost() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?-${limit}=3&_page=${page}`
+  );
+  const data = await res.json();
+
+  return data;
+}
+
+async function showPosts() {
+  const posts = await fetchPost();
+  console.log(posts);
+  posts.forEach(post => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = ` <div class="number">${post.id}</div>
+      <div class="post-info">
+          <h2 class="post-title">Post ${post.id}</h2>
+          <p class="post-body">${post.body}</p>
+      </div>`;
+    postContainer.appendChild(postElement);
+  });
+}
+
+function showLoading() {
+  loading.classList.add("show");
+  setTimeout(() => {
+    loading.classList.remove("show");
+    setTimeout(() => {
+      // page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+showPosts();
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
