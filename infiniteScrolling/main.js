4decc7a8924540cbd87a142a9fdf97fa57f7ae41
Nabil Fannane <nabil@goadroit.com>
2020-02-11 02:42:11 -0800

    last

--- infiniteScrolling/main.js
+++ infiniteScrolling/main.js
@@ -7,17 +7,52 @@ let page = 1;
 
 async function fetchPost() {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?-${limit}=3&_page=${page}`
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
   );
   const data = await res.json();
 
   return data;
 }
 
-function fil(e) {
-  const term = e.target.value.toUpperCase();
-  const posts = document.querySelectorAll(".post");
+async function showPosts() {
+  const posts = await fetchPost();
+  console.log(posts);
   posts.forEach(post => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = ` <div class="number">${post.id}</div>
+        <div class="post-info">
+            <h2 class="post-title">Post ${post.title}</h2>
+            <p class="post-body">${post.body}</p>
+        </div>`;
+    postContainer.appendChild(postElement);
+  });
+}
+showPosts();
+function showLoading() {
+  loading.classList.add("show");
+  setTimeout(() => {
+    loading.classList.remove("show");
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 9) {
+    showLoading();
+    console.log(123);
+  }
+});
+
+filter.addEventListener("input", e => {
+  const term = e.target.value.toUpperCase();
+  const list = document.querySelectorAll(".post");
+  list.forEach(post => {
     const title = post.document
       .querySelector(".post-title")
       .innerText.toUpperCase();
@@ -30,40 +65,4 @@ function fil(e) {
       post.style.display = "none";
     }
   });
-}
-
-async function showPosts() {
-  const posts = await fetchPost();
-  console.log(posts);
-  posts.forEach(post => {
-    const postElement = document.createElement("div");
-    postElement.classList.add("posts");
-    postElement.innerHTML = ` <div class="number">${post.id}</div>
-      <div class="post-info">
-          <h2 class="post-title">Post ${post.id}</h2>
-          <p class="post-body">${post.body}</p>
-      </div>`;
-    postContainer.appendChild(postElement);
-  });
-}
-
-function showLoading() {
-  loading.classList.add("show");
-  setTimeout(() => {
-    loading.classList.remove("show");
-    setTimeout(() => {
-      // page++;
-      showPosts();
-    }, 300);
-  }, 1000);
-}
-showPosts();
-window.addEventListener("scroll", () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-  if (scrollTop + clientHeight >= scrollHeight - 5) {
-    showLoading();
-  }
 });
-
-filter.addEventListener("input", fil);
