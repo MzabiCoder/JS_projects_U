3c1bedb44b6e874c8d7572d8b5a205d7ac64cf17
Nabil Fannane <nabil@goadroit.com>
2020-03-02 05:08:21 -0800

    last

---
+++ cocktailDB/src/pages/Error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const Errore=()=> {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Errore                                                                                                                                                     
+
+
